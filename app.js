3b1d363adfa3826c74d0ad0ced0638fd897818e1
Jeremy Antonoff <jeremyantonoff@gmail.com>
2017-08-28 14:53:40 -0400

    add contact form with ajax functionality

--- app.js
+++ app.js
@@ -1,15 +1,44 @@
-var express = require('express'),
-    app     = express(),
+var express    = require('express'),
+    app        = express(),
     bodyParser = require('body-parser');
-
-
+    nodeMailer = require('nodemailer');
 
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname + "/public"));
 app.use(bodyParser.urlencoded({extended: true}));
 
+
 app.get("/", function(req, res) {
   res.render("index")
+});
+
+app.post("/contact", function(req, res) {
+  const nodemailer = require('nodemailer');
+  let transporter = nodemailer.createTransport({
+      host: "smtp.gmail.com",
+      port: 465,
+      secure: true,
+      auth: {
+          user: process.env.mailerUSER,
+          pass: process.env.mailerPASS
+      }
+  });
+
+  let mailOptions = {
+      from: req.body.name,
+      to: 'northpacificrestaurant@gmail.com',
+      subject: 'North Pacific Contact Form',
+      html: '<h2>' + req.body.name + '<br></h2><h3>' + req.body.phone + '<br><br>' + req.body.message  + '</h3></h2>'
+  };
+
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      res.end('failure');
+    } else {
+    console.log('message-sent')
+    res.end('success');
+    }
+  })
 });
 
 app.listen(3000,function() {
--- public/js/norpac.js
+++ public/js/norpac.js
@@ -1,16 +1,50 @@
+$('#form').submit(function(event) {
+  event.preventDefault();
+  submit();
+})
 
+function submit() {
+  var name = $("#name").val();
+  var phone = $("#phone").val();
+  var message = $("#message").val();
+  var info = {name: name, phone: phone, message: message}
+  $.ajax({
+    type: "POST",
+    url: "http://localhost:3000/contact",
+    data: info,
+    success: function(text) {
+      if (text === "success") {
+        success();
+      } else {
+        failed();
+      }
+    }
+  });
+}
 
+function success() {
+  $("#form").hide();
+  $("#resHeader").hide();
+  $(".message-update").fadeIn();
+  $(".message-update").html('<h2>' + 'YOUR MESSAGE HAS BEEN SENT' + '</h2><p>' + 'We will get back to you shortly!' +'</p>');
+}
 
-
+function failed() {
+  $("#form").hide();
+  $("#resHeader").hide();
+  $(".message-update").addClass("failed")
+  $(".message-update").fadeIn(3000);
+  $(".message-update").html('<h2>' + 'UH OH! SOMETHING WENT WRONG!' + '</h2><p>' + 'Please call us @ 555-555-2849' +'</p>');
+}
 
 function initMap() {
-var uluru = {lat: -25.363, lng: 131.044};
-var map = new google.maps.Map(document.getElementById('map'), {
-  zoom: 4,
-  center: uluru
-});
-var marker = new google.maps.Marker({
-  position: uluru,
-  map: map
-});
+  var uluru = {lat: -25.363, lng: 131.044};
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 4,
+    center: uluru
+  });
+  var marker = new google.maps.Marker({
+    position: uluru,
+    map: map
+  });
 }
