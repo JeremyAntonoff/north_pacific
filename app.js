80a4bd1d19bbda43f63c8bf12d09c0203bd4b2a8
Jeremy Antonoff <jeremyantonoff@gmail.com>
2017-08-30 01:44:40 -0400

    implement authentication and new routes

--- app.js
+++ app.js
@@ -1,11 +1,34 @@
-var express    = require('express'),
-    app        = express(),
-    bodyParser = require('body-parser');
-    nodeMailer = require('nodemailer');
+var express         = require('express'),
+    app             = express(),
+    bodyParser      = require('body-parser'),
+    nodeMailer      = require('nodemailer'),
+    passport        = require('passport'),
+    localStrategy   = require('passport-local'),
+    User            = require('./models/user.js'),
+    express_session = require("express-session"),
+    mongoose        = require('mongoose'),
+    seedUser        = require('./seedUser');
+
+// seedUser();
+mongoose.connect('mongodb://localhost/north_pacific', {useMongoClient: true});
 
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname + "/public"));
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(express_session({
+  secret: 'secret phrases are underrated',
+  resave: false,
+  saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+passport.use(new localStrategy(User.authenticate()));
+app.use(function(req, res, next) {
+  res.locals.user = req.user
+  next();
+})
 
 
 app.get("/", function(req, res) {
@@ -48,6 +71,37 @@ app.post("/contact", function(req, res) {
     }
   })
 });
+
+app.get("/blog", function(req, res) {
+  res.render('blog/index', {page: 'blog'})
+});
+
+app.get('/blog/login', function(req, res) {
+  res.render("blog/login", {page: 'login'})
+});
+
+app.post("/blog/login", passport.authenticate("local", {
+  successRedirect: "/blog",
+  failureRedirect: "/blog/login"
+  }), function(req, res) {
+});
+
+app.get("/blog/logout",function(req, res) {
+  req.logout();
+  res.redirect("/blog")
+});
+
+app.get("/blog/new", isLoggedIn, function(req, res) {
+  res.render("blog/new", {page: 'new'})
+});
+
+function isLoggedIn(req, res, next) {
+  if (req.isAuthenticated()) {
+    next();
+  } else {
+    res.redirect("/blog/login")
+  }
+}
 
 app.listen(3000,function() {
   console.log('server has started')
