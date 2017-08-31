5d9c6121e70ea5b88a1950a58df360fea08a56d8
Jeremy Antonoff <jeremyantonoff@gmail.com>
2017-08-31 15:42:11 -0400

    add routes folder to spread out indexRoutes and blogRoutes

---
+++ routes/blogRoutes.js
@@ -0,0 +1,122 @@
+const express = require('express');
+const router = express.Router();
+const multer = require('multer');
+const Blog = require("../models/blogs");
+const passport = require('passport');
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, './public/imgs/uploads/')
+  },
+  filename: function (req, file, cb) {
+    cb(null, Date.now() + '.jpg')
+  }
+});
+const upload = multer({ storage: storage });
+
+router.get("/", function(req, res) {
+  Blog.find({}, function(err, foundBlogs) {
+    if (err) {
+      req.flash("error", err);
+      res.redirect("/blog");
+    } else {
+      res.render('blog/index', {page: 'blog', blogs: foundBlogs});
+    }
+  });
+});
+
+router.get('/login', function(req, res) {
+  res.render("blog/login", {page: 'login'});
+});
+
+router.post("/login",  passport.authenticate("local", {
+  successRedirect: "/blog",
+  failureRedirect: "/blog/login",
+  failureFlash: true,
+  successFlash: "Welcome back!"
+  }), function(req, res) {
+});
+
+router.get("/logout",function(req, res) {
+  req.logout();
+  req.flash("success", "You have successfully logged out!");
+  res.redirect("/blog");
+});
+
+router.post("/", isLoggedIn, upload.single("image"), function(req, res, next) {
+  if (req.file) {
+    req.body.blog.image = req.file.filename;
+  }
+  Blog.create(req.body.blog, function(err, blog) {
+    if (err) {
+      req.flash("error", "Something went wrong!");
+      res.redirect("/blog");
+    } else {
+      req.flash("success", "Your have successfully created a new blog!");
+      res.redirect("/blog");
+    }
+  });
+});
+
+router.get("/new", isLoggedIn, function(req, res) {
+  res.render("blog/new", {page: 'new'});
+});
+
+router.get("/:id", function(req, res) {
+  Blog.findById(req.params.id, function(err, foundBlog) {
+    if (err) {
+      req.flash("error", "Something went wrong!");
+      res.redirect("/blog");
+    } else {
+      res.render("blog/show", {page: 'blog', blog: foundBlog});
+    }
+  });
+});
+
+router.get("/:id/edit", isLoggedIn, function(req, res) {
+  Blog.findById(req.params.id, function(err, foundBlog) {
+    if (err) {
+      req.flash("error", "Something went wrong!");
+      res.redirect("/blog/" + req.params.id);
+    } else {
+      res.render("blog/edit", {page: 'blog', blog: foundBlog});
+    }
+  });
+});
+
+router.put("/:id", isLoggedIn, upload.single("image"), function(req, res) {
+  if (req.file) {
+    req.body.blog.image = req.file.filename;
+  }
+  Blog.findByIdAndUpdate(req.params.id, req.body.blog, function(err, updatedBlog) {
+    if (err) {
+      req.flash("error", "Something went wrong!");
+      res.redirect("/blog/" + req.params.id);
+    } else {
+      req.flash("success", "Your blog post has been updated!");
+      res.redirect("/blog/" + req.params.id);
+    }
+  });
+});
+
+router.delete("/:id", isLoggedIn, function(req, res) {
+  Blog.findByIdAndRemove(req.params.id, function(err) {
+    if (err) {
+      req.flash("error", "Something went wrong!");
+      res.redirect("/blog/" + req.params.id);
+    } else {
+      req.flash("success", "Your blog post has been deleted!")
+      res.redirect("/blog");
+    }
+  });
+});
+
+function isLoggedIn(req, res, next) {
+  if (req.isAuthenticated()) {
+    next();
+  } else {
+    req.flash("error", "You need to be logged in to do that")
+    res.redirect("/blog/login")
+  }
+};
+
+module.exports = router;
---
+++ routes/indexRoutes.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const router = express.Router();
+
+router.get("/", function(req, res) {
+  res.render("index", {page: 'home'})
+});
+
+router.get("/menu", function(req, res) {
+  res.render("menu", {page: 'menu'})
+});
+
+router.get("/story", function(req, res) {
+  res.render("story", {page: 'story'})
+});
+
+router.post("/contact", function(req, res) {
+  const nodemailer = require('nodemailer');
+  let transporter = nodemailer.createTransport({
+      host: "smtp.gmail.com",
+      port: 465,
+      secure: true,
+      auth: {
+          user: process.env.mailerUSERS,
+          pass: process.env.mailerPASS
+      }
+  });
+
+  let mailOptions = {
+      from: req.body.name,
+      to: 'northpacificrestaurant@gmail.com',
+      subject: 'North Pacific Contact Form',
+      html: '<h2>' + req.body.name + '<br></h2><h3>' + req.body.phone + '<br><br>' + req.body.message  + '</h3></h2>'
+  };
+
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      res.end('failure');
+    } else {
+    console.log('message-sent')
+    res.end('success');
+    }
+  });
+});
+
+module.exports = router;
