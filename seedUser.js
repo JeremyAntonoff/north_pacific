1612e0f1d2e476687661168ceccd5149dce62137
Jeremy Antonoff <jeremyantonoff@gmail.com>
2017-08-31 15:34:01 -0400

    minor change to env variables

--- seedUser.js
+++ seedUser.js
@@ -1,11 +1,11 @@
-var User = require("./models/user.js");
-var mongoose = require('mongoose');
+const User = require("./models/user.js");
+const mongoose = require('mongoose');
 
 function seedUser() {
   User.register(new User({username: process.env.USERNAME}), process.env.PASSWORD, function(err, user) {
     if (err) {
       console.log(err);
     }
-  })
+  });
 }
 module.exports = seedUser;
