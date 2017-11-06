55ab77063340400dc79fd81c7e8c13cb0e2a1a4e
Jeremy Antonoff <jeremyantonoff@gmail.com>
2017-11-05 19:19:17 -0500

    minor es6 and promise refactor

--- public/js/norpac.js
+++ public/js/norpac.js
@@ -4,46 +4,45 @@ $('#form').submit(function(event) {
 });
 
 function submit() {
-  let name = $("#name").val();
-  let phone = $("#phone").val();
-  let message = $("#message").val();
-  let info = {name: name, phone: phone, message: message}
+  let name = $('#name').val();
+  let phone = $('#phone').val();
+  let message = $('#message').val();
+  let info = { name, phone, message };
   $.ajax({
-    type: "POST",
-    url: "/contact",
-    data: info,
-    success: function(text) {
-      if (text === "success") {
-        success();
-      } else {
-        failed();
-      }
-    }
-  });
+    type: 'POST',
+    url: '/contact',
+    data: info
+  })
+    .then(success)
+    .catch(failed);
 }
 
 function success() {
-  $("#form").hide();
-  $("#resHeader").hide();
-  $(".contact-form-message").fadeIn();
-  $(".contact-form-message").html('<h2>' + 'YOUR MESSAGE HAS BEEN SENT' + '</h2><p>' + 'We will get back to you shortly!' +'</p>');
+  $('#form').hide();
+  $('#resHeader').hide();
+  $('.contact-form-message').fadeIn();
+  $('.contact-form-message').html(
+    `<h2>YOUR MESSAGE HAS BEEN SENT</h2><p>We will get back to you shortly!</p>`
+  );
 }
 
 function failed() {
-  $("#form").hide();
-  $("#resHeader").hide();
-  $(".contact-form-message").addClass("contact-form-failed");
-  $(".contact-form-message").fadeIn(3000);
-  $(".contact-form-message").html('<h2>' + 'UH OH! SOMETHING WENT WRONG!' + '</h2><p>' + 'Please call us @ 555-555-2849' +'</p>');
+  $('#form').hide();
+  $('#resHeader').hide();
+  $('.contact-form-message').addClass('contact-form-failed');
+  $('.contact-form-message').fadeIn(3000);
+  $('.contact-form-message').html(
+    `<h2>UH OH! SOMETHING WENT WRONG!</h2><p>Please call us @ 555-555-2849</p>`
+  );
 }
 
 function initMap() {
-  let uluru = {lat: 27.751473, lng: -82.629998};
-  let map = new google.maps.Map(document.getElementById('map'), {
+  const uluru = { lat: 27.751473, lng: -82.629998 };
+  const map = new google.maps.Map(document.getElementById('map'), {
     zoom: 4,
     center: uluru
   });
-  let marker = new google.maps.Marker({
+  const marker = new google.maps.Marker({
     position: uluru,
     map: map
   });
