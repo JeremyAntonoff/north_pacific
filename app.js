ca5ae80493523b6c888a8b38ce25af43ee183dbf
Jeremy Antonoff <jeremyantonoff@gmail.com>
2017-11-05 19:18:15 -0500

    minor es6 refactor

--- app.js
+++ app.js
@@ -1,33 +1,35 @@
-const express       = require('express'),
-    app             = express(),
-    bodyParser      = require('body-parser'),
-    passport        = require('passport'),
-    localStrategy   = require('passport-local'),
-    flash           = require('connect-flash')
-    User            = require('./models/user.js'),
-    Blog            = require('./models/blogs'),
-    express_session = require('express-session'),
-    mongoose        = require('mongoose'),
-    methodOverride  = require('method-override'),
-    seedUser        = require('./seedUser'),
-    port            = process.env.PORT || 3000,
-    url             = process.env.DBURL || 'mongodb://localhost/north_pacific'
+const express = require('express'),
+  app = express(),
+  bodyParser = require('body-parser'),
+  passport = require('passport'),
+  localStrategy = require('passport-local'),
+  flash = require('connect-flash'),
+  User = require('./models/user.js'),
+  Blog = require('./models/blogs'),
+  express_session = require('express-session'),
+  mongoose = require('mongoose'),
+  methodOverride = require('method-override'),
+  seedUser = require('./seedUser'),
+  port = process.env.PORT || 3000,
+  url = process.env.DBURL || 'mongodb://localhost/north_pacific';
 
-const indexRoutes = require("./routes/indexRoutes")
-const blogRoutes  = require("./routes/blogRoutes");
-mongoose.connect(url, {useMongoClient: true});
-// seedUser()
+const indexRoutes = require('./routes/indexRoutes');
+const blogRoutes = require('./routes/blogRoutes');
+mongoose.connect(url, { useMongoClient: true });
+// seedUser() seeds user for blog log in
 app.set('view engine', 'ejs');
-app.use(express.static(__dirname + "/public"));
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(methodOverride("_method"));
+app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(methodOverride('_method'));
 app.use(flash());
 app.locals.moment = require('moment');
-app.use(express_session({
-  secret: 'secret phrases are underrated',
-  resave: false,
-  saveUninitialized: false
-}));
+app.use(
+  express_session({
+    secret: 'secret phrases are underrated',
+    resave: false,
+    saveUninitialized: false
+  })
+);
 
 app.use(passport.initialize());
 app.use(passport.session());
@@ -35,15 +37,15 @@ passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 passport.use(new localStrategy(User.authenticate()));
 
-app.use(function(req, res, next) {
+app.use((req, res, next) => {
   res.locals.user = req.user;
   res.locals.error = req.flash('error');
   res.locals.success = req.flash('success');
   next();
 });
 app.use(indexRoutes);
-app.use("/blog", blogRoutes);
+app.use('/blog', blogRoutes);
 
-app.listen(port,function() {
-  console.log('server has started')
+app.listen(port, () => {
+  console.log('server has started');
 });
